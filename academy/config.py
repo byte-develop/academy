3c8d41fd2af0511ff204e2603b66e0ccf6c56326
byte <137470945+byte-develop@users.noreply.github.com>
2025-08-30 21:03:12 +0300

    Delete academy directory

--- academy/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv('BOT_TOKEN', 'your-telegram-bot-token')
-CRYPTOBOT_TOKEN = os.getenv('CRYPTOBOT_TOKEN', 'your-cryptobot-token')
-CHANNEL_ID = os.getenv('CHANNEL_ID', '@your_channel')
-CHAT_ID = os.getenv('CHAT_ID', '@your_chat')
-ADMIN_ID = int(os.getenv('ADMIN_ID', '123456789'))
